945c3a264041c19394f856c878412ae1e1a510e3
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2023-09-25 23:36:32 +0600

    first commit

---
+++ src/Components/Header/Navbar/Navbar.jsx
@@ -0,0 +1,61 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="mt-6 md:mt-10 w-11/12 mx-auto">
+      <nav className="flex flex-col md:flex-row justify-between items-center">
+        <div>
+          <img className="w-48" src="/src/assets/Logo.png" alt="" />
+        </div>
+        <div>
+          <ul className="flex flex-row gap-4 mt-4 md:mt-0 text-lg">
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive, isPending }) =>
+                  isPending
+                    ? "pending"
+                    : isActive
+                    ? "text-[#FF444A] underline"
+                    : ""
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/Donation"
+                className={({ isActive, isPending }) =>
+                  isPending
+                    ? "pending"
+                    : isActive
+                    ? "text-[#FF444A] underline"
+                    : ""
+                }
+              >
+                Donation
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/Statistics"
+                className={({ isActive, isPending }) =>
+                  isPending
+                    ? "pending"
+                    : isActive
+                    ? "text-[#FF444A] underline"
+                    : ""
+                }
+              >
+                Statistics
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Components/Root/Root";
+import ErrorPage from "./Components/ErrorPage/ErrorPage";
+import Home from "./Components/Home/Home";
+import Donation from "./Components/Donation/Donation";
+import Statistics from "./Components/Statistics/Statistics";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/Donation",
+        element: <Donation></Donation>,
+      },
+      {
+        path: "/Statistics",
+        element: <Statistics></Statistics>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
