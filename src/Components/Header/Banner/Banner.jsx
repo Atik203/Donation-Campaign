a413c69e085b83a1ca1e4e27fcc822d89154db23
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2023-09-25 23:59:49 +0600

    Created Banner Components and also added responsive design of Header section for all devices

---
+++ src/Components/Header/Banner/Banner.jsx
@@ -0,0 +1,39 @@
+const Banner = () => {
+  const background = {
+    backgroundImage: `url("https://i.ibb.co/tPz3tK9/Rectangle-4281.png?fbclid=IwAR3xBaGZ25QE7mfVsX2iYWhhpFQCea1heVhQpRuuNJ23BA4FdafFthgLKZg")`,
+    width: "100%",
+    height: "70vh",
+    transitionProperty: "opacity",
+    transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
+    transitionDuration: "150ms",
+    opacity: "0.2",
+  };
+
+  return (
+    <div className="">
+      <div className="relative" style={background}></div>
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <div>
+          <h1 className="text-xl md:text-3xl lg:text-5xl font-bold text-center mt-32 md:mt-28 lg:mt-0">
+            I Grow By Helping People In Need
+          </h1>
+          <div className="flex justify-center items-center mt-4 md:mt-6 lg:mt-10 mb-10">
+            <form>
+              <input
+                type="search"
+                placeholder="Search here..."
+                id=""
+                className="bg-base-100 border-2 border-[#DEDEDE] px-4 w-48 md:w-80 h-12 md:h-14 rounded-lg"
+              />
+            </form>
+            <button className="btn btn-primary bg-[#FF444A] rounded-lg h-10 md:h-14 px-6 text-white border-none">
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
