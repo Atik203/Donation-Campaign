4aaccf93f55ac421b792b24112464057aa366308
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2023-09-27 20:46:58 +0600

    Added error page design and make responsive for all devices

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -3,21 +3,33 @@ import { Link } from "react-router-dom";
 const ErrorPage = () => {
   const error = useRouteError();
   return (
-    <div
-      id="error-page"
-      className="text-4xl font-bold text-center justify-center mt-40"
-    >
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-      <Link
-        to="/"
-        className="btn btn-secondary text-xl mt-5 bg-gray-500 border-none text-white"
-      >
-        Go Back To Home
-      </Link>
+    <div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
+      <div className="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
+        <div className="relative">
+          <div className="absolute">
+            <div className="">
+              <h1 className="my-2 text-gray-800 font-bold text-2xl">
+                Looks like you've found the doorway to the great nothing
+              </h1>
+              <p className="my-2 text-gray-800">
+                Sorry about that! Please visit our hompage to get where you need
+                to go.
+              </p>
+              <Link to={"/"}>
+                <button className="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">
+                  Take me there!
+                </button>
+              </Link>
+            </div>
+          </div>
+          <div>
+            <img src="https://i.ibb.co/G9DC8S0/404-2.png" />
+          </div>
+        </div>
+      </div>
+      <div>
+        <img src="https://i.ibb.co/ck1SGFJ/Group.png" />
+      </div>
     </div>
   );
 };
